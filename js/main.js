84fd9eae575da4ea22985f0deef21ae2b3dc17ca
Marco GP <anthonydeoz2@gmail.com>
2022-04-05 19:18:40 -0500

    third commit accordion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Slick Slider
+
 $('.cards__container').slick({
     dots: true,
     infinite: true,
@@ -31,5 +33,27 @@ $('.cards__container').slick({
       }
     ]
   });
+
+
+  // Accordion
+
+  const accordions = document.getElementsByClassName("question__item__title");
+
+  for(i = 0; i<accordions.length; i++){
+    accordions[i].addEventListener("click", function(){
+      this.nextSibling.nextSibling.classList.toggle("active")
+
+     const point = this.querySelector(".title__point")
+     point.classList.toggle("rotate")
+
+     if( point.innerHTML == "+"){
+      point.innerHTML =  "-"
+     }else{
+       point.innerHTML = "+"
+     }
+    })
+  }
+
+  
 
 
